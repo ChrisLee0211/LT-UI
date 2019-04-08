b344ebee5e9255de077782a971c969baee2dd4ec
ChrisLee0211 <1018596741@qq.com>
2019-04-08 23:04:35 +0800

    第二次提交

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ class SlideButton implements Params {
     text: string = '请滑动以解锁';
     isTouch: boolean = false;
     isSuccess:boolean = false;
+    isEnd:boolean = false;
     constructor(width: number, height: number, text: string) {
         this.width = width;
         this.height = height;
@@ -25,11 +26,10 @@ class SlideButton implements Params {
 
     //重置样式
     reset(): void {
-        let slide_wrap: any = document.querySelector('.slide-wrap');
-        let slide_bar: any = document.querySelector('.slide-bar');
-        let slide_block: any = document.querySelector('.slide-block');
-        let slide_text:any = document.querySelector('.slide-text');
-        // let slide_block_icon:any = document.querySelector('.slide-block-icon');
+        let slide_wrap: HTMLElement = document.querySelector('.slide-wrap') as HTMLElement;
+        let slide_bar: HTMLElement = document.querySelector('.slide-bar') as HTMLElement;
+        let slide_block: HTMLElement = document.querySelector('.slide-block') as HTMLElement;
+        let slide_text:HTMLElement = document.querySelector('.slide-text') as HTMLElement;
         slide_wrap.style.width = this.width + 'px';
         slide_wrap.style.height = this.height + 'px';
         slide_block.style.width = this.height + 'px';
@@ -37,8 +37,6 @@ class SlideButton implements Params {
         slide_bar.style.lineHeight = this.height + 'px';
         slide_bar.style.fontSize = (this.height / 5) + 'px';
         slide_bar.style.textIndent = (this.height / 2) + "px";
-        // slide_block_icon.style.marginTop = (this.height/4) + "px"
-        // slide_block_icon.style.marginLeft = (this.height/4) + "px"
         slide_text.innerText = this.text
     }
 
@@ -46,10 +44,10 @@ class SlideButton implements Params {
     slideEvent(): void {
         let self = this;
         let slide_max = self.width - self.height;
-        let slide_block: any = document.querySelector('.slide-block');
+        let slide_block: HTMLElement = document.querySelector('.slide-block') as HTMLElement;
         // 手势触碰滑块：改变滑块样式——初始化是否触碰布尔值、是否成功布尔值
         slide_block.addEventListener('touchstart', function () {
-            slide_block.style.backgroundColor = "grey";
+            slide_block.style.backgroundColor = "#f6f3f3";
             self.isTouch = true;
             self.isSuccess = false;
         });
@@ -59,15 +57,16 @@ class SlideButton implements Params {
             if (self.isTouch == true) {
                 e.target.style.left = (parseInt(curX) - self.height/2) + "px";
                 let lastMove = parseInt(e.target.style.left)
-                console.log(lastMove)
                     if (lastMove < 0) {
                         e.target.style.left = 0;
                     }
                     if (lastMove > slide_max) {
                         e.target.style.left = (self.width - self.height) + "px";
+                        self.isEnd = true
                     }
             }
         });
+
         // 手势离开，判定是否到最右
         slide_block.addEventListener('touchend',function(e:any){
             if(parseInt(e.target.style.left) >= slide_max){
@@ -82,6 +81,21 @@ class SlideButton implements Params {
         })
     }
     success():void{
-
+        let slide_wrap: HTMLElement = document.querySelector('.slide-wrap') as HTMLElement;
+        let slide_bar: HTMLElement = document.querySelector('.slide-bar') as HTMLElement;
+        let slide_text:HTMLElement = document.querySelector('.slide-text') as HTMLElement;
+        let slide_block: HTMLElement = document.querySelector('.slide-block') as HTMLElement;
+        let slideWrap_width:String|any = slide_wrap.style.width ;
+        let slideBlock_width:String|any = slide_block.style.width ;
+        let slide_bar_length:number = parseInt(slideWrap_width);  
+        let slide_block_length:number = parseInt(slideBlock_width)
+        slide_text.innerHTML = '';
+        // slide_bar.style.width = slide_block_length + 'px';
+        // slide_bar.style.marginLeft = (slide_bar_length/2 - slide_block_length/2) + 'px';
+        slide_block.style.left =  (slide_bar_length/2 - slide_block_length/2) + 'px'
+        
+        slide_block.className = 'loading-img'
+        slide_block.classList.add("action")
+        
     }
 }
--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var SlideButton = /** @class */ (function () {
         this.text = '请滑动以解锁';
         this.isTouch = false;
         this.isSuccess = false;
+        this.isEnd = false;
         this.width = width;
         this.height = height;
         this.text = text;
@@ -19,7 +20,6 @@ var SlideButton = /** @class */ (function () {
         var slide_bar = document.querySelector('.slide-bar');
         var slide_block = document.querySelector('.slide-block');
         var slide_text = document.querySelector('.slide-text');
-        // let slide_block_icon:any = document.querySelector('.slide-block-icon');
         slide_wrap.style.width = this.width + 'px';
         slide_wrap.style.height = this.height + 'px';
         slide_block.style.width = this.height + 'px';
@@ -27,8 +27,6 @@ var SlideButton = /** @class */ (function () {
         slide_bar.style.lineHeight = this.height + 'px';
         slide_bar.style.fontSize = (this.height / 5) + 'px';
         slide_bar.style.textIndent = (this.height / 2) + "px";
-        // slide_block_icon.style.marginTop = (this.height/4) + "px"
-        // slide_block_icon.style.marginLeft = (this.height/4) + "px"
         slide_text.innerText = this.text;
     };
     // 绑定滑动事件
@@ -38,7 +36,7 @@ var SlideButton = /** @class */ (function () {
         var slide_block = document.querySelector('.slide-block');
         // 手势触碰滑块：改变滑块样式——初始化是否触碰布尔值、是否成功布尔值
         slide_block.addEventListener('touchstart', function () {
-            slide_block.style.backgroundColor = "grey";
+            slide_block.style.backgroundColor = "#f6f3f3";
             self.isTouch = true;
             self.isSuccess = false;
         });
@@ -48,12 +46,12 @@ var SlideButton = /** @class */ (function () {
             if (self.isTouch == true) {
                 e.target.style.left = (parseInt(curX) - self.height / 2) + "px";
                 var lastMove = parseInt(e.target.style.left);
-                console.log(lastMove);
                 if (lastMove < 0) {
                     e.target.style.left = 0;
                 }
                 if (lastMove > slide_max) {
                     e.target.style.left = (self.width - self.height) + "px";
+                    self.isEnd = true;
                 }
             }
         });
@@ -72,6 +70,20 @@ var SlideButton = /** @class */ (function () {
         });
     };
     SlideButton.prototype.success = function () {
+        var slide_wrap = document.querySelector('.slide-wrap');
+        var slide_bar = document.querySelector('.slide-bar');
+        var slide_text = document.querySelector('.slide-text');
+        var slide_block = document.querySelector('.slide-block');
+        var slideWrap_width = slide_wrap.style.width;
+        var slideBlock_width = slide_block.style.width;
+        var slide_bar_length = parseInt(slideWrap_width);
+        var slide_block_length = parseInt(slideBlock_width);
+        slide_text.innerHTML = '';
+        // slide_bar.style.width = slide_block_length + 'px';
+        // slide_bar.style.marginLeft = (slide_bar_length/2 - slide_block_length/2) + 'px';
+        slide_block.style.left = (slide_bar_length / 2 - slide_block_length / 2) + 'px';
+        slide_block.className = 'loading-img';
+        slide_block.classList.add("action");
     };
     return SlideButton;
 }());
