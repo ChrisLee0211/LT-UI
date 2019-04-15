09594f2462b244712ff79a172e6f03cabecf9843
ChrisLee0211 <1018596741@qq.com>
2019-04-15 23:28:25 +0800

    优化结构，完善功能“

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ interface Params {
     width: number;
     height: number;
     text: string;
+    id: string;
+    createDom(): void;
     init(): void
 }
 
@@ -9,12 +11,15 @@ class SlideButton implements Params {
     width: number;
     height: number;
     text: string = '请滑动以解锁';
+    id: string;
     isTouch: boolean = false;
-    isSuccess:boolean = false;
-    isEnd:boolean = false;
-    constructor(width: number, height: number, text: string) {
+    isSuccess: boolean = false;
+    isEnd: boolean = false;
+    isOver:boolean = false;
+    constructor(width: number, height: number, text: string, id: string) {
         this.width = width;
         this.height = height;
+        this.id = id;
         this.text = text
     }
 
@@ -26,42 +31,43 @@ class SlideButton implements Params {
     }
 
     // 创建元素
-    createDom():void{
-        let button = document.getElementsByTagName('lt-slide-button')
-    console.log(button)
-    // 滑块背景
-    let slide_wrap:HTMLElement = document.createElement('div');
-    slide_wrap.setAttribute('id','slide-wrap');
-    slide_wrap.className = 'slide-wrap';
-    // 滑动条
-    let slide_bar:HTMLElement = document.createElement('div');
-    slide_bar.setAttribute('id','slide-bar');
-    slide_bar.className = 'slide-bar';
-    // 滑动条文本
-    let slide_text:HTMLElement = document.createElement('span');
-    slide_text.setAttribute('id','slide-text');
-    slide_text.className = "slide-text"
-    // 滑块
-    let slide_block:HTMLElement = document.createElement('div');
-    slide_block.setAttribute('id','slide-block');
-    slide_block.className = 'slide-block'
+    createDom(): void {
+        let button: HTMLElement = document.getElementById(this.id) as HTMLElement
 
-    let fragment = document.createDocumentFragment()
-    slide_bar.appendChild(slide_text);
-    slide_wrap.appendChild(slide_bar);
-    slide_wrap.appendChild(slide_block);
-    fragment.appendChild(slide_wrap);
-    for(let i = 0; i < button.length; i++){
-        button[i].appendChild(fragment)
-    }
+        // 滑块背景
+        let slide_wrap: HTMLElement = document.createElement('div');
+        slide_wrap.setAttribute('id', this.id + 'slide-wrap');
+        slide_wrap.className = 'slide-wrap';
+        // 滑动条
+        let slide_bar: HTMLElement = document.createElement('div');
+        slide_bar.setAttribute('id', this.id + 'slide-bar');
+        slide_bar.className = 'slide-bar';
+        // 滑动条文本
+        let slide_text: HTMLElement = document.createElement('span');
+        slide_text.setAttribute('id', this.id + 'slide-text');
+        slide_text.className = "slide-text"
+        // 滑块
+        let slide_block: HTMLElement = document.createElement('div');
+        slide_block.setAttribute('id', this.id + 'slide-block');
+        slide_block.className = 'slide-block'
+
+        let fragment = document.createDocumentFragment()
+        slide_bar.appendChild(slide_text);
+        slide_wrap.appendChild(slide_bar);
+        slide_wrap.appendChild(slide_block);
+        fragment.appendChild(slide_wrap);
+        button.appendChild(fragment)
+        // for (let i = 0; i < button.length; i++) {
+        //     button[i].appendChild(fragment)
+        // }
     }
 
     //重置样式
     reset(): void {
-        let slide_wrap: HTMLElement = document.querySelector('#slide-wrap') as HTMLElement;
-        let slide_bar: HTMLElement = document.querySelector('#slide-bar') as HTMLElement;
-        let slide_block: HTMLElement = document.querySelector('#slide-block') as HTMLElement;
-        let slide_text:HTMLElement = document.querySelector('.slide-text') as HTMLElement;
+        let slide_wrap: HTMLElement = document.querySelector('#' + this.id + 'slide-wrap') as HTMLElement;
+        let slide_bar: HTMLElement = document.querySelector('#' + this.id + 'slide-bar') as HTMLElement;
+        let slide_block: HTMLElement = document.querySelector('#' + this.id + 'slide-block') as HTMLElement;
+        let slide_text: HTMLElement = document.querySelector('#' + this.id + 'slide-text') as HTMLElement;
         slide_bar.className = 'slide-bar';
         slide_block.className = 'slide-block'
         slide_text.className = 'slide-text'
@@ -79,85 +85,84 @@ class SlideButton implements Params {
     // 绑定滑动事件
     slideEvent(): void {
         let self = this;
-        let slide_max:number = self.width - self.height;
-        let slide_block: HTMLElement = document.querySelector('#slide-block') as HTMLElement;
+        let slide_max: number = self.width - self.height;
+        let slide_block: HTMLElement = document.querySelector('#' + this.id + 'slide-block') as HTMLElement;
         // 手势触碰滑块：改变滑块样式——初始化是否触碰布尔值、是否成功布尔值
         slide_block.addEventListener('touchstart', function () {
+            self.isSuccess = false;
+            self.isTouch = true;
             slide_block.style.backgroundColor = "#f6f3f3";
             slide_block.style.transition = "left 0s linear"
-            self.isTouch = true;
-            self.isSuccess = false;
         });
         // 手势滑动：计算左右边界
         slide_block.addEventListener('touchmove', function (e: any) {
             let curX: string = e.touches[0].pageX;
-            if (self.isTouch == true) {
-                e.target.style.left = (parseInt(curX) - self.height/2) + "px";
-                let lastMove:number = parseInt(e.target.style.left)
-                    if (lastMove < 0) {
-                        e.target.style.left = 0;
-                    }
-                    if (lastMove > slide_max) {
-                        e.target.style.left = (self.width - self.height) + "px";
-                        self.isEnd = true
-                    }
+            if (self.isOver == false) { //判定是否已经完成了一次滑动并且成功，防止成功后被继续拖动
+                e.target.style.left = (parseInt(curX) - self.height / 2) + "px";
+                let lastMove: number = parseInt(e.target.style.left)
+                if (lastMove < 0) {
+                    e.target.style.left = 0;
+                }
+                if (lastMove > slide_max) {
+                    e.target.style.left = (self.width - self.height) + "px";
+                    self.isEnd = true
+                }
             }
         });
 
         // 手势离开，判定是否到最右
-        slide_block.addEventListener('touchend',function(e:any){
-            if(parseInt(e.target.style.left) >= slide_max){
-                self.isSuccess = true;
-                slide_block.style.backgroundColor = "white";
-                self.success()
-            }else{
-                e.target.style.color = "lightgreen"
-                slide_block.style.backgroundColor = "white";
-                e.target.style.left = 0
+        slide_block.addEventListener('touchend', function (e: any) {
+            if (self.isOver == false) { //判定是否已经完成了一次滑动并且成功，防止成功后被继续拖动
+                if (parseInt(e.target.style.left) >= slide_max) {
+                    self.isSuccess = true;
+                    self.success()
+                } else {
+                    e.target.style.color = "lightgreen"
+                    e.target.style.left = 0
+                }
             }
+            slide_block.style.backgroundColor = "white";
             self.isTouch = false
         })
     }
 
     // 请求成功的方法
-    isPass():void{
-        let slide_block: HTMLElement = document.querySelector('#slide-block') as HTMLElement;
+    isPass(): void {
+        let slide_block: HTMLElement = document.querySelector('#' + this.id + 'slide-block') as HTMLElement;
         slide_block.className = 'success';
-        setTimeout(function():void{
-            slide_block.style.backgroundSize = "50%"
-        },300)
+        // 一旦请求成功，即判定为整个滑动条的动画完成，不再提供滑动（ps：这个逻辑主要用于请求成功后锁定滑块）
+        this.isOver = true;
     }
 
     // 请求失败的方法
-    isFail():void{
-        let slide_bar: HTMLElement = document.querySelector('#slide-bar') as HTMLElement;
-        let slide_block: HTMLElement = document.querySelector('#slide-block') as HTMLElement;
-        let slide_text:HTMLElement = document.querySelector('.slide-text') as HTMLElement;
+    isFail(): void {
+        let slide_bar: HTMLElement = document.querySelector('#' + this.id + 'slide-bar') as HTMLElement;
+        let slide_block: HTMLElement = document.querySelector('#' + this.id + 'slide-block') as HTMLElement;
+        let slide_text: HTMLElement = document.querySelector('#' + this.id + 'slide-text') as HTMLElement;
         slide_block.className = 'fail';
-        slide_block.style.backgroundSize = "50%";
-        slide_block.classList.add('move')
+        slide_block.style.backgroundSize = "50% 50%";
         let self = this
-        setTimeout(function():void{
+        setTimeout(function (): void {
             self.reset()
-        },300)
+        }, 300)
     }
 
     // 加载中的钩子函数
-    loading():any{
+    loading(): any {
         return
     }
 
-    success():void{
-        let slide_wrap: HTMLElement = document.querySelector('#slide-wrap') as HTMLElement;
-        let slide_bar: HTMLElement = document.querySelector('#slide-bar') as HTMLElement;
-        let slide_text:HTMLElement = document.querySelector('.slide-text') as HTMLElement;
-        let slide_block: HTMLElement = document.querySelector('#slide-block') as HTMLElement;
-        let slideWrap_width:String|any = slide_wrap.style.width ;
-        let slideBlock_width:String|any = slide_block.style.width ;
-        let slide_bar_length:number = parseInt(slideWrap_width);  
-        let slide_block_length:number = parseInt(slideBlock_width)
+    success(): void {
+        let slide_wrap: HTMLElement = document.querySelector('#' + this.id + 'slide-wrap') as HTMLElement;
+        let slide_bar: HTMLElement = document.querySelector('#' + this.id + 'slide-bar') as HTMLElement;
+        let slide_text: HTMLElement = document.querySelector('#' + this.id + 'slide-text') as HTMLElement;
+        let slide_block: HTMLElement = document.querySelector('#' + this.id + 'slide-block') as HTMLElement;
+        let slideWrap_width: String | any = slide_wrap.style.width;
+        let slideBlock_width: String | any = slide_block.style.width;
+        let slide_bar_length: number = parseInt(slideWrap_width);
+        let slide_block_length: number = parseInt(slideBlock_width)
         slide_text.innerHTML = '';
-        slide_block.style.left =  (slide_bar_length/2 - slide_block_length/2) + 'px'
+        slide_block.style.left = (slide_bar_length / 2 - slide_block_length / 2) + 'px'
         slide_bar.classList.add("active")
         slide_block.className = 'loading'
         slide_block.style.transition = "left .5s linear"
--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 "use strict";
 var SlideButton = /** @class */ (function () {
-    function SlideButton(width, height, text) {
+    function SlideButton(width, height, text, id) {
         this.text = '请滑动以解锁';
         this.isTouch = false;
         this.isSuccess = false;
         this.isEnd = false;
+        this.isOver = false;
         this.width = width;
         this.height = height;
+        this.id = id;
         this.text = text;
     }
     // 初始化各个部分的样式和绑定事件
@@ -17,39 +19,39 @@ var SlideButton = /** @class */ (function () {
     };
     // 创建元素
     SlideButton.prototype.createDom = function () {
-        var button = document.getElementsByTagName('lt-slide-button');
-        console.log(button);
+        var button = document.getElementById(this.id);
         // 滑块背景
         var slide_wrap = document.createElement('div');
-        slide_wrap.setAttribute('id', 'slide-wrap');
+        slide_wrap.setAttribute('id', this.id + 'slide-wrap');
         slide_wrap.className = 'slide-wrap';
         // 滑动条
         var slide_bar = document.createElement('div');
-        slide_bar.setAttribute('id', 'slide-bar');
+        slide_bar.setAttribute('id', this.id + 'slide-bar');
         slide_bar.className = 'slide-bar';
         // 滑动条文本
         var slide_text = document.createElement('span');
-        slide_text.setAttribute('id', 'slide-text');
+        slide_text.setAttribute('id', this.id + 'slide-text');
         slide_text.className = "slide-text";
         // 滑块
         var slide_block = document.createElement('div');
-        slide_block.setAttribute('id', 'slide-block');
+        slide_block.setAttribute('id', this.id + 'slide-block');
         slide_block.className = 'slide-block';
         var fragment = document.createDocumentFragment();
         slide_bar.appendChild(slide_text);
         slide_wrap.appendChild(slide_bar);
         slide_wrap.appendChild(slide_block);
         fragment.appendChild(slide_wrap);
-        for (var i = 0; i < button.length; i++) {
-            button[i].appendChild(fragment);
-        }
+        button.appendChild(fragment);
+        // for (let i = 0; i < button.length; i++) {
+        //     button[i].appendChild(fragment)
+        // }
     };
     //重置样式
     SlideButton.prototype.reset = function () {
-        var slide_wrap = document.querySelector('#slide-wrap');
-        var slide_bar = document.querySelector('#slide-bar');
-        var slide_block = document.querySelector('#slide-block');
-        var slide_text = document.querySelector('.slide-text');
+        var slide_wrap = document.querySelector('#' + this.id + 'slide-wrap');
+        var slide_bar = document.querySelector('#' + this.id + 'slide-bar');
+        var slide_block = document.querySelector('#' + this.id + 'slide-block');
+        var slide_text = document.querySelector('#' + this.id + 'slide-text');
         slide_bar.className = 'slide-bar';
         slide_block.className = 'slide-block';
         slide_text.className = 'slide-text';
@@ -67,18 +69,24 @@ var SlideButton = /** @class */ (function () {
     SlideButton.prototype.slideEvent = function () {
         var self = this;
         var slide_max = self.width - self.height;
-        var slide_block = document.querySelector('#slide-block');
+        var slide_block = document.querySelector('#' + this.id + 'slide-block');
         // 手势触碰滑块：改变滑块样式——初始化是否触碰布尔值、是否成功布尔值
         slide_block.addEventListener('touchstart', function () {
+            self.isSuccess = false;
+            self.isTouch = true;
             slide_block.style.backgroundColor = "#f6f3f3";
             slide_block.style.transition = "left 0s linear";
-            self.isTouch = true;
-            self.isSuccess = false;
+            // let middle: string = ((self.width / 2 - self.height) + self.height / 2) + 'px'
+            // if (slide_block.style.left == middle) {
+            //     self.isTouch = false
+            // } else {
+            //     self.isTouch = true;
+            // }
         });
         // 手势滑动：计算左右边界
         slide_block.addEventListener('touchmove', function (e) {
             var curX = e.touches[0].pageX;
-            if (self.isTouch == true) {
+            if (self.isOver == false) {
                 e.target.style.left = (parseInt(curX) - self.height / 2) + "px";
                 var lastMove = parseInt(e.target.style.left);
                 if (lastMove < 0) {
@@ -92,35 +100,37 @@ var SlideButton = /** @class */ (function () {
         });
         // 手势离开，判定是否到最右
         slide_block.addEventListener('touchend', function (e) {
-            if (parseInt(e.target.style.left) >= slide_max) {
-                self.isSuccess = true;
-                slide_block.style.backgroundColor = "white";
-                self.success();
+            if (self.isOver == false) {
+                if (parseInt(e.target.style.left) >= slide_max) {
+                    self.isSuccess = true;
+                    self.success();
+                }
+                else {
+                    e.target.style.color = "lightgreen";
+                    e.target.style.left = 0;
+                }
             }
-            else {
-                e.target.style.color = "lightgreen";
-                slide_block.style.backgroundColor = "white";
-                e.target.style.left = 0;
-            }
+            slide_block.style.backgroundColor = "white";
             self.isTouch = false;
         });
     };
     // 请求成功的方法
     SlideButton.prototype.isPass = function () {
-        var slide_block = document.querySelector('#slide-block');
+        var slide_block = document.querySelector('#' + this.id + 'slide-block');
         slide_block.className = 'success';
-        setTimeout(function () {
-            slide_block.style.backgroundSize = "50%";
-        }, 300);
+        this.isOver = true;
+        //  slide_block.style.transition = "background-size .1s linear"
+        // setTimeout(function (): void {
+        //     slide_block.style.backgroundSize = "50% 50%"
+        // }, 1100)
     };
     // 请求失败的方法
     SlideButton.prototype.isFail = function () {
-        var slide_bar = document.querySelector('#slide-bar');
-        var slide_block = document.querySelector('#slide-block');
-        var slide_text = document.querySelector('.slide-text');
+        var slide_bar = document.querySelector('#' + this.id + 'slide-bar');
+        var slide_block = document.querySelector('#' + this.id + 'slide-block');
+        var slide_text = document.querySelector('#' + this.id + 'slide-text');
         slide_block.className = 'fail';
-        slide_block.style.backgroundSize = "50%";
-        slide_block.classList.add('move');
+        slide_block.style.backgroundSize = "50% 50%";
         var self = this;
         setTimeout(function () {
             self.reset();
@@ -131,10 +141,10 @@ var SlideButton = /** @class */ (function () {
         return;
     };
     SlideButton.prototype.success = function () {
-        var slide_wrap = document.querySelector('#slide-wrap');
-        var slide_bar = document.querySelector('#slide-bar');
-        var slide_text = document.querySelector('.slide-text');
-        var slide_block = document.querySelector('#slide-block');
+        var slide_wrap = document.querySelector('#' + this.id + 'slide-wrap');
+        var slide_bar = document.querySelector('#' + this.id + 'slide-bar');
+        var slide_text = document.querySelector('#' + this.id + 'slide-text');
+        var slide_block = document.querySelector('#' + this.id + 'slide-block');
         var slideWrap_width = slide_wrap.style.width;
         var slideBlock_width = slide_block.style.width;
         var slide_bar_length = parseInt(slideWrap_width);
